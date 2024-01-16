dc7a351ec55c1b1c4d32556e8d0b192aa001a0ce
mohammad.afshar <mohammad.afshar@partdp.ai>
2024-01-16 11:35:58 +0330

    hide startup message

--- backend/chainlit/hello.py
+++ backend/chainlit/hello.py
@@ -5,8 +5,9 @@ from chainlit import AskUserMessage, Message, on_chat_start
 
 @on_chat_start
 async def main():
-    res = await AskUserMessage(content="What is your name?", timeout=30).send()
-    if res:
-        await Message(
-            content=f"Your name is: {res['output']}.\nChainlit installation is working!\nYou can now start building your own chainlit apps!",
-        ).send()
+    pass
+    # res = await AskUserMessage(content="What is your name?", timeout=30).send()
+    # if res:
+        # await Message(
+        #     content=f"Your name is: {res['output']}.\nChainlit installation is working!\nYou can now start building your own chainlit apps!",
+        # ).send()
